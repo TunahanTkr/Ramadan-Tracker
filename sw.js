f41f885fc522ee649fb6a2df02d7388034f8a141
TunahanTkr <162555850+TunahanTkr@users.noreply.github.com>
2026-02-18 20:53:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,37 @@
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-      caches.open("ramadan-tracker-v3").then((cache) =>
-        cache.addAll(["./", "./index.html", "./manifest.json"])
+const CACHE_NAME = "ramadan-tracker-v5";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
       )
-    );
-  });
-  
-  self.addEventListener("fetch", (e) => {
-    e.respondWith(
-      caches.match(e.request).then((res) => res || fetch(e.request))
-    );
-  });
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
